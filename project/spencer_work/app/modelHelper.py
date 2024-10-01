e6bba12f4695778866865478e32955495ef657cc
sdgarrett98 <spencerdgarrett@outlook.com>
2024-10-01 17:29:22 -0400

    working webapp

--- project/spencer_work/app/modelHelper.py
+++ project/spencer_work/app/modelHelper.py
@@ -2,27 +2,41 @@ import pandas as pd
 import pickle
 import numpy as np
 
-class ModelHelper():
+class ModelHelper:
     def __init__(self):
         pass
 
-    def makePredictions(self, sex_flag, age, bmi, alcohol_consumption, physical_activity,
-                        diet_quality, sleep_quality, systolic_bp, diastolic_bp, 
-                        fasting_blood_sugar, hba1c, serum_creatinine, bun_levels, 
-                        gfr, protein_in_urine, gender, smoking, family_history_kidney_disease,
-                        family_history_hypertension, family_history_diabetes, previous_aki,
-                        uti, kidney_stones, hypertension, diabetes, obesity, 
-                        heavy_metals_exposure, occupational_exposure_chemicals, water_quality,
-                        ethnicity, socioeconomic_status, education_level):
-        
+    def makePredictions(
+        self, age, gender, ethnicity, socioeconomic_status, education_level, bmi,
+        smoking, alcohol_consumption, physical_activity, diet_quality, sleep_quality,
+        family_history_kidney_disease, family_history_hypertension, family_history_diabetes,
+        previous_acute_kidney_injury, urinary_tract_infections, systolic_bp, diastolic_bp,
+        fasting_blood_sugar, hba1c, serum_creatinine, bun_levels, gfr, protein_in_urine,
+        acr, serum_electrolytes_sodium, serum_electrolytes_potassium, serum_electrolytes_calcium,
+        serum_electrolytes_phosphorus, hemoglobin_levels, cholesterol_total, cholesterol_ldl,
+        cholesterol_hdl, cholesterol_triglycerides, ace_inhibitors, diuretics, nsaids_use, 
+        statins, antidiabetic_medications, edema, fatigue_levels, nausea_vomiting, muscle_cramps, 
+        itching, quality_of_life_score, heavy_metals_exposure, occupational_exposure_chemicals,
+        water_quality, medical_checkups_frequency, medication_adherence, health_literacy
+    ):
         # create dataframe of one row for inference
         df = pd.DataFrame({
             "Age": [age],
+            "Gender": [gender],
+            "Ethnicity": [ethnicity],
+            "SocioeconomicStatus": [socioeconomic_status],
+            "EducationLevel": [education_level],
             "BMI": [bmi],
+            "Smoking": [smoking],
             "AlcoholConsumption": [alcohol_consumption],
             "PhysicalActivity": [physical_activity],
             "DietQuality": [diet_quality],
             "SleepQuality": [sleep_quality],
+            "FamilyHistoryKidneyDisease": [family_history_kidney_disease],
+            "FamilyHistoryHypertension": [family_history_hypertension],
+            "FamilyHistoryDiabetes": [family_history_diabetes],
+            "PreviousAcuteKidneyInjury": [previous_acute_kidney_injury],
+            "UrinaryTractInfections": [urinary_tract_infections],
             "SystolicBP": [systolic_bp],
             "DiastolicBP": [diastolic_bp],
             "FastingBloodSugar": [fasting_blood_sugar],
@@ -31,30 +45,38 @@ class ModelHelper():
             "BUNLevels": [bun_levels],
             "GFR": [gfr],
             "ProteinInUrine": [protein_in_urine],
-            "Gender": [gender],
-            "Smoking": [smoking],
-            "FamilyHistoryKidneyDisease": [family_history_kidney_disease],
-            "FamilyHistoryHypertension": [family_history_hypertension],
-            "FamilyHistoryDiabetes": [family_history_diabetes],
-            "PreviousAcuteKidneyInjury": [previous_aki],
-            "UrinaryTractInfections": [uti],
-            "KidneyStones": [kidney_stones],
-            "Hypertension": [hypertension],
-            "Diabetes": [diabetes],
-            "Obesity": [obesity],
+            "ACR": [acr],
+            "SerumElectrolytesSodium": [serum_electrolytes_sodium],
+            "SerumElectrolytesPotassium": [serum_electrolytes_potassium],
+            "SerumElectrolytesCalcium": [serum_electrolytes_calcium],
+            "SerumElectrolytesPhosphorus": [serum_electrolytes_phosphorus],
+            "HemoglobinLevels": [hemoglobin_levels],
+            "CholesterolTotal": [cholesterol_total],
+            "CholesterolLDL": [cholesterol_ldl],
+            "CholesterolHDL": [cholesterol_hdl],
+            "CholesterolTriglycerides": [cholesterol_triglycerides],
+            "ACEInhibitors": [ace_inhibitors],
+            "Diuretics": [diuretics],
+            "NSAIDsUse": [nsaids_use],
+            "Statins": [statins],
+            "AntidiabeticMedications": [antidiabetic_medications],
+            "Edema": [edema],
+            "FatigueLevels": [fatigue_levels],
+            "NauseaVomiting": [nausea_vomiting],
+            "MuscleCramps": [muscle_cramps],
+            "Itching": [itching],
+            "QualityOfLifeScore": [quality_of_life_score],
             "HeavyMetalsExposure": [heavy_metals_exposure],
             "OccupationalExposureChemicals": [occupational_exposure_chemicals],
             "WaterQuality": [water_quality],
-            "Ethnicity": [ethnicity],
-            "SocioeconomicStatus": [socioeconomic_status],
-            "EducationLevel": [education_level]
+            "MedicalCheckupsFrequency": [medical_checkups_frequency],
+            "MedicationAdherence": [medication_adherence],
+            "HealthLiteracy": [health_literacy]
         })
 
-        # model
+        # Load the model
         model = pickle.load(open("Chronic_Kidney_Dsease_Model_Pipeline.h5", 'rb'))
 
-        # columns in order
-        df = df.loc[:, ['Pclass', 'Sex', 'Age', 'Fare', 'Embarked', 'Has_Cabin', 'Family_Size']]
-
+        # Make predictions
         preds = model.predict_proba(df)
-        return(preds[0][1])
+        return preds[0][1]
