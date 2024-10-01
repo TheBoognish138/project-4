070d45b168ed9927d55da5bce982667faf3ae241
sdgarrett98 <spencerdgarrett@outlook.com>
2024-10-01 17:58:33 -0400

    added rounding and more text to prediction output

--- project/spencer_work/app/static/js/logic.js
+++ project/spencer_work/app/static/js/logic.js
@@ -135,9 +135,9 @@ function makePredictions() {
             var prob = parseFloat(returnedData["prediction"]);
 
             if (prob > 0.5) {
-                $("#output").text(`You are diagnosed with CKD with probability ${prob}.`);
+                $("#output").text(`Based on the information entered, there is a ${Math.round(prob * 100).toFixed(2)}% probability that you may be diagnosed with CKD. Please consult a healthcare professional for further testing and advice.`);
             } else {
-                $("#output").text(`You are not diagnosed with CKD with probability ${prob}.`);
+                $("#output").text(`Great news! Based on the information entered, there is a ${Math.round(prob * 100).toFixed(2)}% probability that you may be diagnosed with CKD. Please consult a healthcare professional for further testing and advice.`);
             }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
